25335c2cfeed0dc8fb37c383f95b64b776b07b12
Shiro Tang <jxt598@case.edu>
2023-12-03 19:37:02 -0500

    Improve Frontend

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <dev id = 'App'>
+  <img alt="Vue logo" src="./assets/logo2.jpg" class="logo">
+  <PostComponent />
+</dev>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostComponent from './components/PostComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PostComponent
   }
 }
 </script>
@@ -22,5 +24,28 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#App {
+  text-align: center;
+}
+
+.logo {
+  max-width: 200px; /* 设置最大宽度 */
+  max-height: 200px; /* 设置最大高度 */
+  width: auto; /* 保持宽高比例 */
+  height: auto; /* 保持宽高比例 */
+}
+
+#App::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: url('./assets/logo2.jpg') center/cover no-repeat; /* 设置背景图 */
+  opacity: 0.5; /* 设置半透明度 */
+  z-index: -1; /* 将背景图放在最底层 */
 }
 </style>
---
+++ src/components/PostComponent.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="container">
+    <h1>Welcome to WG!</h1>
+    <div class="create-post">
+      <label for="create-post">Latest News!?</label>
+      <input type="text" id="create-post" v-model="text" placeholder="Create a post">
+      <button v-on:click="createPost">Post</button>
+    </div>
+    <p class="error" v-if="error">{{ error }}</p>
+    <div class="post-container">
+      <div
+        class="post"
+        v-for="(post, index) in posts"
+        v-bind:item="post"
+        v-bind:index="index"
+        v-bind:key="post._id"
+        v-on:dblclick="deletePost(post._id)"
+      >
+        <div class="post-header">
+          <span class="created-at">{{ formatDate(post.createdAt) }}</span>
+        </div>
+        <p class="text">{{ post.text }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostService from '../PostService';
+
+export default {
+  name: 'PostComponent',
+  data() {
+    return {
+      posts: [],
+      error: '',
+      text: '',
+    };
+  },
+  async created() {
+    try {
+      this.posts = await PostService.getPosts();
+    } catch (err) {
+      this.error = err.message;
+    }
+  },
+  methods: {
+    async createPost() {
+      await PostService.insertPost(this.text);
+      this.posts = await PostService.getPosts();
+    },
+    async deletePost(id) {
+      await PostService.deletePost(id);
+      this.posts = await PostService.getPosts();
+    },
+    formatDate(date) {
+      return `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+  div.container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 30px;
+  }
+
+  h1 {
+    text-align: center;
+    color: #ffffff;
+    text-shadow: 2px 2px 2px rgb(0, 0, 0);
+  }
+
+  .create-post {
+    margin-bottom: 20px;
+    display: flex; /* 使用 Flex 布局 */
+    align-items: center; /* 垂直居中对齐 */
+  }
+
+  label {
+    margin-right: 10px;
+    font-weight: bold; /* 加粗 Latest News */
+  }
+
+  input {
+    padding: 8px;
+    margin-right: 10px;
+    flex: 1; /* 拉长输入框，占据剩余空间 */
+  }
+
+  button {
+    padding: 8px 16px;
+    background-color: #007bff; /* 修改为蓝色 */
+    color: white;
+    border: none;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #0056b3; /* 修改为深蓝色 */
+  }
+
+  p.error {
+    border: 1px solid #ff5b5f;
+    background-color: #ffc5c1;
+    padding: 10px;
+    margin-bottom: 15px;
+    color: #d9534f;
+  }
+
+  .post {
+    position: relative;
+    border: 1px solid #0d5097; /* 修改为蓝色 */
+    background-color: rgb(208, 245, 248);
+    padding: 10px;
+    margin-bottom: 15px;
+  }
+
+  .post-header {
+    background-color: rgba(0, 10, 100, 0.623);
+    color: white;
+    padding: 5px;
+    font-size: 13px;
+    margin-bottom: 10px;
+  }
+
+  p.text {
+    font-size: 16px;
+    margin-bottom: 0;
+  }
+</style>
+
